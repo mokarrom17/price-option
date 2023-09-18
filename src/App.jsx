27542395e0d91c8cc7fe8f1d4fb5899c03e464b4
mokarrom17 <mokarrombasher@gmail.com>
2023-09-18 23:26:29 +0600

    setup tellwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-6xl text-center mt-5 text-fuchsia-500'>Vite + React</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Nabvar/Nabvar.jsx
@@ -0,0 +1,10 @@
+
+const Nabvar = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Nabvar;
